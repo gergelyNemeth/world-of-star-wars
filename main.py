af35fa86bd8733f89f99745b71a15ee7989a2c48
Gergely Németh <ngerle@gmail.com>
2017-06-28 00:40:55 +0200

    Add next and previous page buttons

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import requests
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def root():
-    response = requests.get('https://swapi.co/api/planets/').json()
+@app.route('/', methods=["GET", "POST"])
+def planets(url="https://swapi.co/api/planets/"):
+    if request.method == "POST":
+        if request.form['url'] != "None":
+            url = request.form['url']
+    response = requests.get(url).json()
     planets = response["results"]
-    return render_template("star_wars.html", planets=planets, len=len, int=int, format=format)
+    prev_page = response["previous"]
+    next_page = response["next"]
 
+    return render_template("star_wars.html", planets=planets, len=len, int=int, format=format,
+                           prev_page=prev_page, next_page=next_page)
 
 if __name__ == '__main__':
     app.run(debug=True)
