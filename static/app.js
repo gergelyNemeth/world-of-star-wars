b5cdca1f8c1d6e9674e7b2cae9612cad73a442a1
Gergely Németh <ngerle@gmail.com>
2017-06-29 18:42:58 +0200

    Add content to modal with ajax

--- static/app.js
+++ static/app.js
@@ -5,5 +5,50 @@ $(document).ready(function(){
         $('#planetResidents').on('show.bs.modal', function(e){
             $('.modal-title').text('Residents of ' + planetName);
         })
+        
+        var modalTableHeader = 
+            `<thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Height</th>
+                    <th>Mass</th>
+                    <th>Hair color</th>
+                    <th>Skin color</th>
+                    <th>Eyes color</th>
+                    <th>Birth year</th>
+                    <th>Gender</th>
+                </tr>
+            </thead>`;
+        var residentsTable = modalTableHeader + '<tbody id="modal-content"></tbody>'
+        $('.modal-body').children().detach(); /* remove content if exists*/
+        $('<table>', {
+            'class': 'table table-bordered',
+            'id': 'residentsTable',
+            html:  residentsTable
+        }).appendTo('.modal-body');
+
+        $.getJSON(planetUrl, function(data){
+            var keyList = ['name', 'height', 'mass', 'skin_color', 'hair_color', 'eye_color', 'birth_year', 'gender'] 
+            var items = '';
+            $.each(data.residents, function(key, val) {
+                $.getJSON(val, function(data){
+                    $.each(data, function(key, val){
+                        if ($.inArray(key, keyList) > -1) {
+                            if (key === 'height') {
+                                val = parseFloat(Number(val) / 100) + ' m';
+                            }
+                            if (key === 'mass' && val !== 'unknown') {
+                                val += ' kg'
+                            }
+                            items += ('<td>' + val + '</td>');
+                        }
+                    })
+                    $('<tr>', {
+                        html: items
+                    }).appendTo('#modal-content')
+                    items = '';
+                })
+            });
+        });
     })
 })
