94fd82ce4190a08bf9c245431024d499f890aa79
Gergely Németh <ngerle@gmail.com>
2017-06-30 15:30:02 +0200

    Hide menu items by login status

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.secret_key = '\xbd\x82\x83\xcf\xda}{\xff\xd5\xb8\n\x0cs\xe4\x8e\nU\xfc5\xec0
 
 @app.route('/', methods=["GET", "POST"])
 def planets(url="https://swapi.co/api/planets/"):
-    login_message = session_status()
+    login_message, login_status = session_status()
     if request.method == "POST":
         if request.form['url'] != "None":
             url = request.form['url']
@@ -19,12 +19,13 @@ def planets(url="https://swapi.co/api/planets/"):
     next_page = response["next"]
 
     return render_template("star_wars.html", planets=planets, len=len, int=int, format=format,
-                           prev_page=prev_page, next_page=next_page, login_message=login_message)
+                           prev_page=prev_page, next_page=next_page,
+                           login_message=login_message, login_status=login_status)
 
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
-    login_message = session_status()
+    login_message, login_status = session_status()
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -33,12 +34,13 @@ def registration():
         data_manager.write_user(username, pass_hash)
         return redirect(url_for('planets'))
 
-    return render_template("register_login.html", form="register", login_message=login_message)
+    return render_template("register_login.html", form="register",
+                           login_message=login_message, login_status=login_status)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    login_message = session_status()
+    login_message, login_status = session_status()
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -63,12 +65,15 @@ def logout():
 
 
 def session_status():
+    status = {}
     if 'username' in session:
         login_message = 'Logged in as {}'.format(session['username'])
+        status['logged_in'] = True
     else:
         login_message = 'You are not logged in'
+        status['logged_in'] = False
 
-    return login_message
+    return login_message, status['logged_in']
 
 
 if __name__ == '__main__':
