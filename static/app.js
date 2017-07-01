991a384b6925703979c90c86b471abb52aa1e825
Gergely Németh <ngerle@gmail.com>
2017-07-01 12:07:56 +0200

    Change http requests to https

--- static/app.js
+++ static/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.residents').on('click', function(e){
         var planetName = e.target.dataset.planet;
-        var planetUrl = e.target.dataset.url;
+        var planetUrl = e.target.dataset.url.replace('http://', 'https://');
         $('#planetResidents').on('show.bs.modal', function(e){
             $('.modal-title').text('Residents of ' + planetName);
         })
