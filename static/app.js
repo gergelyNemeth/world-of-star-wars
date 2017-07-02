ca0ae27dc8e138000a40c836f7b699f1d66f4a96
Gergely Németh <ngerle@gmail.com>
2017-07-02 23:57:31 +0200

    Error handling in residents modal

--- static/app.js
+++ static/app.js
@@ -6,42 +6,44 @@ $(document).ready(function(){
             $('.modal-title').text('Residents of ' + planetName);
         })
         
-        var modalTableHeader = 
-            `<thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Height</th>
-                    <th>Mass</th>
-                    <th>Hair color</th>
-                    <th>Skin color</th>
-                    <th>Eyes color</th>
-                    <th>Birth year</th>
-                    <th>Gender</th>
-                </tr>
-            </thead>`;
-        var residentsTable = modalTableHeader + '<tbody id="modal-content"></tbody>'
         /* remove content if exists*/
-        $('.modal-body').children().detach();
-        $('<table>', {
-            'class': 'table table-bordered',
-            'id': 'residentsTable',
-            html:  residentsTable
-        }).appendTo('.modal-body');
+        $('.modal-body').children().remove();
+
+        $('<div>', {
+            class: 'container',
+            id: 'loading',
+            html: 'Loading...'
+        }).appendTo('.modal-body')
 
         $.getJSON(planetUrl, function(data){
+            var modalTableHeader = 
+                `<thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Height</th>
+                        <th>Mass</th>
+                        <th>Hair color</th>
+                        <th>Skin color</th>
+                        <th>Eyes color</th>
+                        <th>Birth year</th>
+                        <th>Gender</th>
+                    </tr>
+                </thead>`;
+            var residentsTable = modalTableHeader + '<tbody id="modal-content"></tbody>'
+
             var keyList = ['name', 'height', 'mass', 'skin_color', 'hair_color', 'eye_color', 'birth_year', 'gender'] 
             var items = '';
 
-            $('<div>', {
-                class: 'container',
-                id: 'loading',
-                html: 'Loading...'
-            }).appendTo('.modal-body')
+            $('#loading').remove();
+            $('<table>', {
+                'class': 'table table-bordered',
+                'id': 'residents-table',
+                html:  residentsTable
+            }).appendTo('.modal-body');
 
             $.each(data.residents, function(key, value) {
                 value = value.replace('http://', 'https://');
                 $.getJSON(value, function(data){
-                    $('#loading').remove()
                     $.each(data, function(key, value){
                         if ($.inArray(key, keyList) > -1) {
                             if (key === 'height') {
@@ -59,6 +61,14 @@ $(document).ready(function(){
                     items = '';
                 })
             });
+        })
+        .fail(function() {
+            $('#loading').remove();
+            $('<div>', {
+                class: 'container',
+                id: 'connection-error',
+                html: '<p>Cannot connect to Star Wars API.</p><p>Please try it later.</p>'
+            }).appendTo('.modal-body')
         });
     })
 })
